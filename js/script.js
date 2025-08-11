c8a6ed1dc5d8ed4c279b542a6a311472c2663051
Luiz Augusto Oliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2025-08-11 11:35:40 -0300

    adiciona funcionalidade do assistente integrado

--- js/script.js
+++ js/script.js
@@ -65,3 +65,107 @@ galeria.forEach(eventosGaleria);
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+// Assistente de recomendação
+const apiKeyInput = document.getElementById("apiKey");
+const localSelect = document.getElementById("localSelect");
+const questionInput = document.getElementById("questionInput");
+const askButton = document.getElementById("askButton");
+const aiResponse = document.getElementById("aiResponse");
+const form = document.getElementById("form");
+
+const markdownToHTML = (text) => {
+  const converter = new showdown.Converter();
+  return converter.makeHtml(text);
+};
+
+const perguntarAI = async (question, local, apiKey) => {
+  const model = "gemini-2.5-flash";
+  const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
+  const pergunta = `
+    ## Especialidade
+    Você é um especialista em recomendar atividades turísticas para pessoas que estão visitando o local ${local}
+
+    ## Tarefa
+    Você deve responder as perguntas do usuário com base no seu conhecimento em lugares legais para se conhecer como restaurantes, cartões postais da cidade, pontos turísticos ou atividades interessantes que podem ser feitas na determinada cidade
+
+    ## Regras
+    - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+    - Se a pergunta não está relacionada ao local, responda com 'Essa pergunta não está relacionada ao local'
+    - Considere a data atual ${new Date().toLocaleDateString()}
+    - Faça pesquisas atualizadas sobre as recomendações, baseado na data atual, para dar uma resposta coerente.
+    - Nunca responda itens que você não tenha certeza de que existe no lugar atualmente.
+
+    ## Resposta
+    - Seja direto e responda no máximo 500 caracteres
+    - Responda em markdown
+    - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+    
+    ---
+    Aqui está a pergunta do usuário: ${question}
+  `;
+
+  const contents = [
+    {
+      role: "user",
+      parts: [
+        {
+          text: pergunta,
+        },
+      ],
+    },
+  ];
+
+  const tools = [
+    {
+      google_search: {},
+    },
+  ];
+
+  // chamada API
+  const response = await fetch(geminiURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      contents,
+      tools,
+    }),
+  });
+
+  const data = await response.json();
+  return data.candidates[0].content.parts[0].text;
+};
+
+const enviarFormulario = async (event) => {
+  event.preventDefault();
+  const apiKey = apiKeyInput.value;
+  const local = localSelect.value;
+  const question = questionInput.value;
+
+  if (apiKey == "" || local == "" || question == "") {
+    alert("Por favor, preencha todos os campos");
+    return;
+  }
+
+  askButton.disabled = true;
+  askButton.textContent = "Perguntando...";
+  askButton.classList.add("loading");
+
+  try {
+    const text = await perguntarAI(question, local, apiKey);
+    aiResponse.querySelector(".response-content").innerHTML =
+      markdownToHTML(text);
+    aiResponse.classList.remove("hidden");
+  } catch (error) {
+    console.log("Erro: ", error);
+  } finally {
+    askButton.disabled = false;
+    askButton.textContent = "Perguntar";
+    askButton.classList.remove("loading");
+  }
+};
+
+form.addEventListener("submit", enviarFormulario);
+
